4833af3193535c27a1f922e7217daa44e650a427
dhruvberserker <95343534+dhruvberserker@users.noreply.github.com>
2021-12-03 19:40:07 +0530

    Add files via upload

---
+++ finding greater number among two numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int a,b;
+    printf("Enter two numbers");
+    scanf("%d%d,&a,&b");
+    if (a>b)
+    printf("number is %d",a);
+    else
+    printf("number is %d",b);
+    return 0;
+}
