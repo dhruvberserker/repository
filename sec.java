0d92635fbea17671a5281790d9f624747bfc4e75
DHRUV TRIVEDI <95343534+drvcodenta@users.noreply.github.com>
2022-04-08 12:02:12 +0530

    Add files via upload

---
+++ sec.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.io.*;
+import java.util.*;
+
+public class sec {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner file = new Scanner(System.in);
+        System.out.print("Enter the string:");
+        String str=file.nextLine();
+        System.out.print("You have entered:"+str);
+    }
+}
