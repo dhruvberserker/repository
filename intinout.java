b73aec3ab04241d129013118f0caa4b7cfdc3d1f
DHRUV TRIVEDI <95343534+drvcodenta@users.noreply.github.com>
2022-04-08 22:51:26 +0530

    Add files via upload

---
+++ intinout.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class intinout {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value of a:");
+        int a= sc.nextInt();
+        System.out.print("Enter value of b:");
+        int b= sc.nextInt();
+        System.out.print("Enter value of c:");
+        int c= sc.nextInt();
+
+        System.out.println("a is " +a);
+        System.out.println("b is " +b);
+        System.out.println("c is " +c);
+    }
+}
